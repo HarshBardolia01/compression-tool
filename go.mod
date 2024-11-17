1af8514b6384f51055bfff2da0bd048eb7c72209
HarshBardolia01 <harshbardolia02@gmail.com>
2024-11-17 15:51:04 +0530

    feat: updated multiset

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/HarshBardolia01/compression-tool
 
 go 1.22.9
 
-require github.com/HarshBardolia01/go-data-structures v0.0.0-20241117062522-d7eb09b3af37
+require github.com/HarshBardolia01/go-data-structures v0.0.0-20241117075115-608545ea968d
--- internal/multiset/multiset.go
+++ internal/multiset/multiset.go
@@ -1,11 +1,14 @@
 package multiset
 
 import (
+	"fmt"
+
 	rbt "github.com/HarshBardolia01/go-data-structures/redblacktree"
 )
 
 type Multiset[K comparable, V any] struct {
 	tree *rbt.Tree[K, V]
+	size int
 }
 
 func NewMultiset[K comparable, V any](comp func(k1, k2 K) int) *Multiset[K, V] {
@@ -15,10 +18,45 @@ func NewMultiset[K comparable, V any](comp func(k1, k2 K) int) *Multiset[K, V] {
 }
 
 func (ms *Multiset[K, V]) Insert(key K, val V) {
+	ms.size++
 	ms.tree.Insert(key, val)
 }
 
 func (ms *Multiset[K, V]) EraseFirst() {
+	ms.size--
 	node := ms.tree.Root.GetLeftmostNode()
 	ms.tree.Erase(node.Key)
+}
+
+func (ms *Multiset[K, V]) Size() int {
+	return ms.size
+}
+
+func (ms *Multiset[K, V]) Begin() V {
+	return ms.tree.Root.GetLeftmostNode().Value
+}
+
+func (ms *Multiset[K, V]) GetTreeSize() int {
+	return ms.tree.Len()
+}
+
+func (ms *Multiset[K, V]) Print() {
+	var dfs func(node *rbt.Node[K, V])
+
+	dfs = func(node *rbt.Node[K, V]) {
+		if node.IsNil() {
+			return
+		}
+
+		dfs(node.Left)
+		fmt.Printf("%d: %v\n", node.Key, node.Value)
+		dfs(node.Right)
+	}
+
+	if ms.tree.Len() == 0 {
+		fmt.Println("Empty tree")
+		return
+	}
+
+	dfs(ms.tree.Root)
 }
